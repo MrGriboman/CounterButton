7d888307959c53dc4631d35f554048f7aef27dd9
MrGriboman <96828637+MrGriboman@users.noreply.github.com>
2022-09-26 12:47:48 +1000

    counter button added

--- app/src/main/java/com/example/counterbutton/MainActivity.kt
+++ app/src/main/java/com/example/counterbutton/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.counterbutton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.counterbutton.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.apply {
+            btnPlusOne.setOnClickListener {
+                val newValue = tvCount.text.toString().toInt() + 1
+                tvCount.text = newValue.toString()
+            }
+        }
     }
 }
