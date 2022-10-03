82c896f884b7a1da3bde7c0774f6058e6c659481
MrGriboman <96828637+MrGriboman@users.noreply.github.com>
2022-10-03 13:15:06 +1000

    random text color

--- app/src/main/java/com/example/counterbutton/SecondActivity.kt
+++ app/src/main/java/com/example/counterbutton/SecondActivity.kt
@@ -1,8 +1,10 @@
 package com.example.counterbutton
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.counterbutton.databinding.ActivitySecondBinding
+import java.util.*
 
 class SecondActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySecondBinding
@@ -13,5 +15,10 @@ class SecondActivity : AppCompatActivity() {
 
         val counterValue = intent.getIntExtra("counterValue", 0)
         binding.tvNumber.text = (0..counterValue).random().toString()
+
+        val rnd = Random()
+        val textColor = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256))
+        binding.tvNumber.setTextColor(textColor)
+
     }
 }
